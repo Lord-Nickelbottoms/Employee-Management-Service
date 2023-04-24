cbe4daf29c19b4a48ef7f055beeed9ad53a7ac78
Nizaam Haffejee <nizaam.h@icloud.com>
2023-04-24 13:41:53 +0200

    added an insert page

--- Employee_Mangement_Service/src/app/app.module.ts
+++ Employee_Mangement_Service/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateInsertComponent } from './pages/create-insert/create-insert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    CreateInsertComponent
   ],
   imports: [
     BrowserModule,
--- Employee_Mangement_Service/src/app/pages/create-insert/create-insert.component.spec.ts
+++ Employee_Mangement_Service/src/app/pages/create-insert/create-insert.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { CreateInsertComponent } from './create-insert.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('CreateInsertComponent', () => {
+  let component: CreateInsertComponent;
+  let fixture: ComponentFixture<CreateInsertComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ CreateInsertComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(CreateInsertComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
