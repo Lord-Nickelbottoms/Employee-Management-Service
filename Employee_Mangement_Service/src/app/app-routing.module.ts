e352fcbe8bb3ec8ef6347b3f6e8abd33072ca013
NyikoCamango <217026744@student.uj.ac.za>
2023-04-24 11:21:10 +0200

    Done with the footer component

--- Employee_Mangement_Service/src/app/app-routing.module.ts
+++ Employee_Mangement_Service/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './component/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
