3b41991be2a1d7fea8710a82d16b5898903bf9cc
Nizaam Haffejee <nizaam.h@icloud.com>
2023-04-24 11:47:29 +0200

    created and styled the Create/Insert page

--- Employee_Mangement_Service/src/app/app-routing.module.ts
+++ Employee_Mangement_Service/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CreateInsertComponent } from './pages/create-insert/create-insert.component';
+
+const routes: Routes = [
+  { path: '', component: CreateInsertComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Employee_Mangement_Service/src/app/app.module.ts
+++ Employee_Mangement_Service/src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateInsertComponent } from './pages/create-insert/create-insert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateInsertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
